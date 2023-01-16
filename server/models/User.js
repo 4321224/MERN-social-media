30ebc16b079c9920e95d0f61ea2e5d4b7e50ffac
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-16 16:03:19 +0700

    model user

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: Strung,
+      required: true,
+      min: 5,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewed: Number,
+    impressions: Nummber,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
