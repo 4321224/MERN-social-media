111d069e341483aa900717e718582f790490bc5b
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-16 13:33:30 +0700

    adding auth middleware

--- server/app.js
+++ server/app.js
@@ -1,26 +1,34 @@
-import express from 'express'
-import cors from "cors"
-import dotenv from "dotenv"
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import multer from 'multer'
-import helmet from "helmet"
-import morgan from 'morgan'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "20mb", extends: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config()
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json({limit: "20mb", extends: true}))
-app.use(bodyParser.urlencoded({limit: "20mb", extended: true}))
-
-
-const PORT = 
+mongoose.set("strictQuery", false);
+const PORT = process.env.PORT || 8080;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
