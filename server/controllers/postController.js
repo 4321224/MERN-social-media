e44ced8846e6a725cfc82476d1566f60b850af48
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-16 14:10:56 +0700

    read post

---
+++ server/controllers/postController.js
@@ -0,0 +1,10 @@
+import Post from "../models/Post";
+
+export const getPost = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,7 +1,10 @@
-import express from "express"
+import express from "express";
+import { verifyToken } from "../middleware/auth";
+import { getPost } from "../controllers/postController";
 
-const router = express.Router()
+const router = express.Router();
 
+router.use(verifyToken);
+router.get("/", getPost);
 
-
-export default router
+export default router;
