75342a46f05115170ead496ac2fefdc38330bad2
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-16 16:25:26 +0700

    create post by user

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,5 +1,28 @@
 import Post from "../models/Post";
+import User from "../models/User";
 
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+    const post = await Post.find();
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 export const getPost = async (req, res) => {
   try {
     const post = await Post.find();
@@ -17,3 +40,10 @@ export const getPostId = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+export const likePost = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth";
-import { getPost } from "../controllers/postController";
+import { createPost, getPost, getPostId } from "../controllers/postController";
 
 const router = express.Router();
 
 router.use(verifyToken);
 router.get("/", getPost);
-
+router.post("/:userId", createPost);
+router.get("/:userId/posts", getPostId);
 
 export default router;
